c202e62a064a1451c5fcb770f2e18edfdeab5cb7
Lance Axel Gasmen <lancegasmen@gmail.com>
2025-06-06 23:00:19 +0800

    Update GameObjects.h

--- GameObjects.h
+++ GameObjects.h
@@ -1,6 +1,7 @@
 #pragma once
 
-enum PlayerState {
+enum PlayerState 
+{
     IDLE,
     RUNNING,
     JUMPING
@@ -10,10 +11,10 @@ class Object {
 private:
     float x, y;
 public:
-    float getX() { return x; }
-    float getY() { return y; }
-    void setX(float xval) { x = xval; }
-    void setY(float yval) { y = yval; }
+    float getX() {return x;}
+    float getY() {return y;}
+    void setX(float xval) {x = xval;}
+    void setY(float yval) {y = yval;}
 };
 
 class Player {
@@ -25,17 +26,17 @@ private:
     float prevX = 0, prevY = 0;
 
 public:
-    // Position methods
-    float getX() { return position.getX(); }
-    float getY() { return position.getY(); }
-    void setX(float xval) { position.setX(xval); }
-    void setY(float yval) { position.setY(yval); }
-    float getPrevX() { return prevX; }
+    //position methds
+    float getX() {return position.getX();}
+    float getY() {return position.getY();}
+    void setX(float xval) {position.setX(xval);}
+    void setY(float yval) {position.setY(yval);}
+    float getPrevX() { return prevX;}
     float getPrevY() { return prevY; }
     void setPrevX(float x) { prevX = x; }
     void setPrevY(float y) { prevY = y; }
 
-    // State methods
+    //state methods
     bool getIsJumping() { return isJumping; }
     bool getIsBottom() { return isBottom; }
     bool getIsFalling() { return isFalling; }
@@ -45,13 +46,14 @@ public:
     void setIsFalling(bool falling) { isFalling = falling; }
     void setIsMoving(bool moving) { isMoving = moving; }
 
-    // Score methods
+    //score methods
     void setScore(int s) { score = s; }
     int getScore() { return score; }
 
-    // Animation state methods
+    //animation state methods
     PlayerState getCurrentState() { return currentState; }
-    void setCurrentState(PlayerState state) {
+    void setCurrentState(PlayerState state) 
+    {
         previousState = currentState;
         currentState = state;
     }
